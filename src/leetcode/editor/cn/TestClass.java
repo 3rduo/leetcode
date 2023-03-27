9df45812aae0cb2fc9c0835be9b00876b47304ed
sunzx <sunzx@lianditech.com>
2023-03-27 09:38:14 +0800

    --  develop  commit

---
+++ src/leetcode/editor/cn/TestClass.java
@@ -0,0 +1,11 @@
+package leetcode.editor.cn;
+
+/**
+ * @author 3rduo
+ * @create 2023-03-27 1:37
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("develop");
+    }
+}
